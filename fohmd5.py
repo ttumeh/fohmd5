285d15ef99ef2a299cf42c549194a702cce1ec50
thaaltos <tuomas_aaltonen@hotmail.fi>
2023-07-26 20:37:15 +0300

    modify dict attack output to intervals

--- fohmd5.py
+++ fohmd5.py
@@ -18,23 +18,30 @@ Crack MD5 hash using dictionary
 """
 
 
-def crack_md5_dict(hash_list, dictionary_path):
+def crack_md5_dict(hash_list, dictionary_path, interval):
 
     print(f"Cracking {hash_list}.....")
     print(f"Using dictionary from {dictionary_path}.....")
+    calc = 1
+    start_time = time.time()
+    start_time2 = time.time()
 
-    with open(dictionary_path, "r") as dictionary_file:
-        
+    with open(dictionary_path, "r", encoding='utf8') as dictionary_file:
+        to_go = len(dictionary_file.readlines())
         dictionary_file.seek(0)
         # Iterate through the dictionary file line by line
         for index, line in enumerate(dictionary_file, start=1):
+            if calc != 1 and (time.time() - start_time2) > float(interval):
+                print_progress(calc, ''.join(line), start_time, to_go)
+                start_time2 = time.time()
             password = line.strip()
             hashed_pass = md5_hash(password)
             if hashed_pass == hash_list:
                 print(f"\n\nHash {hash_list} cracked: {password}\n")
                 return
-            
-    print("\n\nPassword not found. Try another dictionary.\n")
+            calc += 1
+
+    print("\n\nPassword not found. Maybe try another dictionary?\n")
 
 
 """
@@ -42,7 +49,6 @@ Crack MD5 hash using brute-force
 """
 
 
-# TODO length, wildcards, time
 def crack_md5_brute(hash_list, length, max_length, interval):
 
     to_go = int(math.factorial(100)/math.factorial((100-int(max_length))))
@@ -53,7 +59,7 @@ def crack_md5_brute(hash_list, length, max_length, interval):
     for string_length in range(int(length), int(max_length) + 1):
         mystring = ["0"] * string_length
 
-        # Infinite loop until user cancels or pass found
+        # Brute loop
         while True:
             if calc != 1 and (time.time() - start_time2) > float(interval):
                 print_progress(calc, ''.join(mystring), start_time, to_go)
@@ -66,7 +72,7 @@ def crack_md5_brute(hash_list, length, max_length, interval):
                 c = "".join(mystring)
                 hashed_pass = md5_hash(c)
                 if str(hashed_pass) == str(hash_list):
-                    print(f"\n\nHash cracked: {c}\n")
+                    print(f"\n\nHash {hash_list} cracked: {c}\n")
                     return
 
             # Increment the digits from right to left
@@ -82,7 +88,7 @@ def crack_md5_brute(hash_list, length, max_length, interval):
                     break
             else:
                 break
-    print("\n\nCracking unsuccessful.\n")
+    print("\n\nPassword not found.\n")
 
 
 """
@@ -92,9 +98,9 @@ Print progress of the running process
 
 def print_progress(iteration, mystring, start_time, to_go):
     elapsed_time = round(time.time() - start_time,3)
-    progress=round((iteration/to_go)*100,3)
+    progress=round((iteration/to_go)*100,2)
     print(
-        f"Testing combination: {mystring}  Iteration: {iteration}/{to_go}   Elapsed time: {elapsed_time}    Progress: {progress}%"
+        f"Testing password: {mystring}  Iteration: {iteration}/{to_go}   Elapsed time: {elapsed_time} seconds   Progress: {progress}%"
         )
 
 
@@ -173,7 +179,7 @@ def main():
         hash_value = args.hash
 
     if args.attack == "dict":
-        crack_md5_dict(hash_value, args.attack, args.dictionary)
+        crack_md5_dict(hash_value, args.dictionary, args.interval)
 
     if args.attack == "brute":
         crack_md5_brute(hash_value, args.length, args.maxlength, args.interval)
